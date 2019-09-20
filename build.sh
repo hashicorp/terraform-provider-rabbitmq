3d1a38a31ca018d2ef5344276bbff1b69c9d6014
Samuel Dumont <samuel.dumont@keytradebank.com>
2019-09-20 08:57:13 +0200

    Fixing the build

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ PKG=github.com/samueldumont/$PROJECT_NAME
 VERSION=${MAJOR}.${MINOR}.${PATCH}
 
 for arch in linux darwin; do
-  GOFLAGS=-mod=vendor GO111MODULE=on GOOS=$arch GOARCH=amd64 go build -o "bin/$PROJECT_NAME-v$VERSION-$arch-amd64" .
+  GOFLAGS=-mod=vendor GO111MODULE=on GOOS=$arch GOARCH=amd64 go build -o "bin/$PROJECT_NAME" .
+  zip -r -j "bin/${PROJECT_NAME}_v${VERSION}_${arch}_amd64.zip" bin/${PROJECT_NAME}
 done
 
-GOFLAGS=-mod=vendor GO111MODULE=on CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o "bin/$PROJECT_NAME-v$VERSION-windows-amd64.exe" .
+GOFLAGS=-mod=vendor GO111MODULE=on CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 CGO_ENABLED=1 go build -o "bin/$PROJECT_NAME" .
+zip -r -j "bin/${PROJECT_NAME}_v${VERSION}_windows_amd64.zip" bin/${PROJECT_NAME}
